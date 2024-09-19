e5f00f33fc36b2b2aa4be23be4a9dad439bd9364
Shubham75511 <shubhamkharche1410@gmail.com>
2024-09-19 18:05:26 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log('Connected to MongoDB!');
+    }
+).catch(
+    (err) => {
+        console.log('err');
+    }
+);
 
 const app = express(); // create app
 
@@ -7,4 +20,7 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 }
 );
-// install nodemon - to save changes instantly
+
+//install nodemon - to save changes instantly
+
+//Now connecting server to database using mongodb/mangoose
