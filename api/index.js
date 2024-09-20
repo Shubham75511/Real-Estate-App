6b8f80456c457b2c67a2e7cdc4bd786c37ce8af8
Shubham75511 <shubhamkharche1410@gmail.com>
2024-09-20 16:54:50 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,26 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
-    () => {
-        console.log('Connected to MongoDB!');
-    }
-).catch(
-    (err) => {
-        console.log('err');
-    }
-);
+//Now connecting server to database using mongodb/mangoose
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 
 const app = express(); // create app
 
 //listen on port 3000
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-}
-);
+  console.log("Server is running on port 3000!");
+});
 
 //install nodemon - to save changes instantly
 
-//Now connecting server to database using mongodb/mangoose
+
+//create api route
+
+app.use('/api/user', userRouter);
