9e8ba7603c1256a01053ecd7c69f61f17a383468
Shubham75511 <shubhamkharche1410@gmail.com>
2025-06-15 15:21:09 +0530

    create update user API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 //Now connecting server to database using mongodb/mangoose
@@ -18,6 +19,8 @@ mongoose
 const app = express(); // create app
 
 app.use(express.json());
+
+app.use(cookieParser()); // to parse cookies
 
 //listen on port 3000
 app.listen(3000, () => {
