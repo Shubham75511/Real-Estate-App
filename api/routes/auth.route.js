82ff1d60c3bc8f11a5951416f723382e9f8ceabc
Shubham75511 <shubhamkharche1410@gmail.com>
2025-06-16 00:14:07 +0530

    add create listing API route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { signup } from "../controllers/auth.controller.js";
 import { signin } from "../controllers/auth.controller.js";
 import { google } from "../controllers/auth.controller.js";
+import { signOut } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
