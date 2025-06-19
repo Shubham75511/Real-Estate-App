e046f31e741a7170167237b0cce8ef761cce7a8d
Shubham75511 <shubhamkharche1410@gmail.com>
2025-06-19 10:17:01 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter); // api test softwares ex like postman, insomnia etc.
 app.use("/api/listing", listingRouter); // add listing route
 
-app.use(express.static(path.join(__dirname, "../client/dist"))); // serve static files from client/dist
+app.use(express.static(path.join(__dirname, "/client/dist"))); // serve static files from client/dist
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // serve index.html for all other routes
