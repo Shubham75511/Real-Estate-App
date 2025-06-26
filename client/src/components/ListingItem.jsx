e43372b9b062a6857b9b15ac3de6abf6bb476125
Shubham75511 <shubhamkharche1410@gmail.com>
2025-06-26 15:16:44 +0530

    commit

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+    <div className="bg-textMain shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
@@ -19,21 +19,21 @@ export default function ListingItem({ listing }) {
           </p>
           <div className="flex items-center gap-1">
             <MdLocationOn className="h-4 w-4 text-green-700" />
-            <p className="text-sm text-gray-600 truncate w-full">
+            <p className="text-sm text-gray-700 truncate w-full">
               {listing.address}
             </p>
           </div>
           <p className="text-sm text-gray-600 line-clamp-2">
             {listing.description}
           </p>
-          <p className="text-slate-500 mt-2 font-semibold ">
+          <p className="text-slate-600 mt-2 font-semibold ">
             $
             {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
           </p>
-          <div className="text-slate-700 flex gap-4">
+          <div className="text-slate-600 flex gap-4">
             <div className="font-bold text-xs">
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} beds `
